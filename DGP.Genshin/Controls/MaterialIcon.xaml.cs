9490c42aaf5419a39b310e28d13f0af4fbab00e9
Lightczx <1686188646@qq.com>
2021-01-25 15:06:07 +0800

    image resized

---
+++ DGP.Genshin/Controls/MaterialIcon.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DGP.Genshin.Controls
+{
+    /// <summary>
+    /// MaterialIcon.xaml 的交互逻辑
+    /// </summary>
+    public partial class MaterialIcon : UserControl
+    {
+        public MaterialIcon()
+        {
+            InitializeComponent();
+        }
+        public uint Star
+        {
+            get { return (uint)GetValue(StarProperty); }
+            set { SetValue(StarProperty, value); }
+        }
+        public static readonly DependencyProperty StarProperty =
+            DependencyProperty.Register("Star", typeof(uint), typeof(MaterialIcon), new PropertyMetadata(1));
+
+        public string MaterialName
+        {
+            get { return (string)GetValue(MaterialNameProperty); }
+            set { SetValue(MaterialNameProperty, value); }
+        }
+        public static readonly DependencyProperty MaterialNameProperty =
+            DependencyProperty.Register("MaterialName", typeof(string), typeof(MaterialIcon), new PropertyMetadata(""));
+    }
+}
--- DGP.Genshin/Data/Material.cs
+++ DGP.Genshin/Data/Material.cs
@@ -4,19 +4,7 @@ namespace DGP.Genshin.Data
 {
     public class Material
     {
-        public string Name { get; private set; }
-        public Uri ImageUri { get; private set; }
-
-        public Material(string name, string uri)
-            : this(name, new Uri(uri, UriKind.Relative))
-        {
-
-        }
-
-        public Material(string name, Uri uri)
-        {
-            Name = name;
-            ImageUri = uri;
-        }
+        public string Name { get; set; }
+        public Uri ImageUri { get; set; }
     }
 }
